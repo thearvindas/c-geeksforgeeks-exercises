afff04bd74ec19313d6f05195da2811d048ca854
thearvindas <thearvindas@gmail.com>
2026-06-28 19:33:41 -0600

    untrack compiled binaries, add .gitignore

---
+++ q13arraymaxandmin/main.c
@@ -0,0 +1,24 @@
+// Q13: Write a Program to find the maximum and minimum of an Array.
+
+/*
+Input: arr[] = {4, 7, 2, 1, 9}
+
+Output: Minimum element is: 1
+Maximum element is: 9
+*/
+
+#include <stdio.h>
+
+int main() {
+
+    int array[100];
+
+    printf("Enter the array elements. Type -1 to finish: ");
+
+    while (count < 100) {
+        if (count >= 100 || )
+    }
+
+
+    return 0;
+}
